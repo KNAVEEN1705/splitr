f0db2d3104dd377f37b08066aad2bc5d5b7fef6d
Naveen K <naveennavi1711@gmail.com>
2025-05-14 10:11:42 +0530

    Added new tables and auth

--- convex/schema.js
+++ convex/schema.js
@@ -1,8 +1,8 @@
-
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
+  // Users
   users: defineTable({
     name: v.string(),
     email: v.string(),
@@ -13,4 +13,56 @@ export default defineSchema({
     .index("by_email", ["email"])
     .searchIndex("search_name", { searchField: "name" })
     .searchIndex("search_email", { searchField: "email" }),
+
+  // Expenses
+  expenses: defineTable({
+    description: v.string(),
+    amount: v.string(),
+    category: v.optional(v.string()),
+    date: v.number(),
+    paidByUserId: v.id("users"),
+    splitType: v.string(),
+    splits: v.array(
+      v.object({
+        userId: v.id("users"),
+        amount: v.number(),
+        paid: v.boolean(),
+      })
+    ),
+    groupId: v.optional(v.id("groups")),
+    createdBy: v.id("users"),
+  })
+    .index("by_group", ["groupId"])
+    .index("by_user_and_group", ["paidByUserId", "groupId"])
+    .index("by_date", ["date"]),
+
+  // Settlements
+  settlements: defineTable({
+    amount: v.number(),
+    note: v.optional(v.string()),
+    date: v.number(), // timestamp
+    paidByUserId: v.id("users"),
+    receivedByUserId: v.id("users"),
+    groupId: v.optional(v.id("groups")), // null for one-one settlement
+    relatedExpensedIds: v.optional(v.array(v.id("expenses"))), // Which expenses this settlement covers
+    createdBy: v.id("users"),
+  })
+    .index("by_group", ["groupId"])
+    .index("by_user_and_group", ["paidByUserId", "groupId"])
+    .index("by_receiver_and_group", ["receivedByUserId", "groupId"])
+    .index("by_date", ["date"]),
+
+  // Groups
+  groups: defineTable({
+    name: v.string(),
+    description: v.optional(v.string()),
+    createdBy: v.id("users"), // Reference to users table
+    members: v.array(
+      v.object({
+        userId: v.id("users"), // Reference to users table
+        role: v.string(), // Could validate using v.literal if needed
+        joinedAt: v.number(),
+      })
+    ),
+  }),
 });
